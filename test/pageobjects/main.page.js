d8d2115606aa8cc11743887d61ad68f134c9a4cd
DouglasOttoSf <douglas.davila@objectedge.com>
2023-04-27 15:53:20 -0300

    Commenting code inside main.page

--- test/pageobjects/main.page.js
+++ test/pageobjects/main.page.js
@@ -5,6 +5,7 @@ const Page = require('./page');
  */
 class Main extends Page {
 
+    // CREATE YOUR SELECTORS HERE ↓ //
     get btnId () {
         return $(`//a[@id="idExample"]`);
     }
@@ -54,63 +55,62 @@ class Main extends Page {
     }
 
     get dropdownSelect() {
-        return $('//select');
+        return $('//select'); //creates the selector for the dropdown. 
     }
 
+    // CREATE YOUR FUNCTIONS HERE ↓ //
     async selectFromDropdown() {
-        const log1 = await this.dropdownSelect.getValue();
-        console.log(`LOG MESSAGE 1: Dropdown Value is ${log1}`);
-        await this.dropdownSelect.selectByAttribute('value', 'opel');
-        const log2 = await this.dropdownSelect.getValue();
-        console.log(`LOG MESSAGE 2: Dropdown Value is ${log2}`);
+        const log1 = await this.dropdownSelect.getValue(); //Creates a variable to store the value of the dropdown, BEFORE changing it
+        console.log(`LOG MESSAGE 1: Dropdown Value is ${log1}`); //Adds a console message printing the DEFAULT value of the dropdown ("Volvo")
+        await this.dropdownSelect.selectByAttribute('value', 'opel'); //Selects a new value from the dropdown, considering the attribute "value" and the attribute's value "opel"
+        const log2 = await this.dropdownSelect.getValue(); //Creates a variable to store the value of the dropdown, AFTER it was changed
+        console.log(`LOG MESSAGE 2: Dropdown Value is ${log2}`); //Adds a console message printing the CURRENT value of the dropdown ("Opel")
     }
 
     async clickCheckboxes() {
-        await this.checkBoxCar.click();
-        await this.checkBoxBike.click();
-        await expect(this.checkBoxCar).toBeSelected();
-        await expect(this.checkBoxBike).toBeSelected();
+        await this.checkBoxCar.click(); //Clicks on the checkbox CAR
+        await this.checkBoxBike.click(); //Clicks on the checkbox BIKE
+        await expect(this.checkBoxCar).toBeSelected(); //Expects the checkbox CAR to be selected. If not selected, test will fail.
+        await expect(this.checkBoxBike).toBeSelected(); //Expects the checkbox BIKE to be selected. If not selected, test will fail.
         await browser.pause(5000);
     }
 
+    //Clicks one of the radio buttons (Male, Female, Other)
     async clickRadioBtn(){
         await this.radioMaleSelector.click();
-        await expect(this.radioMaleSelector).toBeSelected();
+        await expect(this.radioMaleSelector).toBeSelected(); //Expects the radioBox element to be selected. If not selected, test will fail.
         await browser.pause(5000);
     }
 
     async emailMeFunction () {
-        await this.inputUsername.setValue('Douglas');
-        await this.inputEmail.setValue('douglas.davila@objectedge.com');
+        await this.inputUsername.setValue('Douglas'); //Sets the value of the input field "Name" to "Douglas"
+        await this.inputEmail.setValue('douglas.davila@objectedge.com'); //Sets the value of the input field "Email" to "douglas.davila@objectedge"
+        await browser.pause(5000); //Pauses the browser for 5 seconds - Good for debugging
+        await this.btnEmailMe.click(); //Clicks on the button "Email Me"
         await browser.pause(5000);
-        await this.btnEmailMe.click();
-        await browser.pause(5000);
-        await expect(this.confMessage).toHaveText('Thanks for contacting us'); 
-        await expect(this.confMessage).toBeDisplayed(); 
+        await expect(this.confMessage).toHaveText('Thanks for contacting us'); //Expects the confirmation message to have the text "Thanks for contacting us". If not, test will fail.
+        await expect(this.confMessage).toBeDisplayed(); //Expects the confirmation message to be displayed. If not, test will fail.
     
-        if (this.confMessage.isDiplayed() == true) {
+        //Not necessary, only for learning purposes ↓
+        //If the confirmation message is displayed, then expects the text to be "Thanks for contacting us". 
+        //If message is displayed, and the text is different, then test will fail. 
+        //If message is not displayed, assertion inside IF will not execute and test will NOT fail.
+        if (this.confMessage.isDiplayed() == true) { 
             await expect(this.confMessage).toHaveText('Thanks for contacting us');    
         }
 
-        //TO CHECK NEXT SESSION - OBJECT PROMISSE
-        /* let confirmationMessage = this.confMessage.getText();
-        console.log(`The confirmation message is: ${confirmationMessage}`); */
+        //Prints the text of the confirmation message
+        const confirmationMessage = await this.confMessage.getText(); //Creates a const to store the text of the confirmation message
+        console.log(`The confirmation message is: ${confirmationMessage}`); //Prints the confirmation message
 
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    open (parameter) {
-        return super.open(parameter);
-    }
-
+    //Creates a function to click on all buttons on top of the page
     async clickAllBtnOnTop(baseUrl, btnSuccess, linkSuccess) {
-        //const baseUrl = "https://ultimateqa.com";  
-        await this.btnId.click();
-        await expect(browser).toHaveUrl(`${baseUrl}${btnSuccess}`);
-        await browser.pause(3000);
-        await browser.back();
+        await this.btnId.click(); //Clicks on the button with ID "idExample"
+        await expect(browser).toHaveUrl(`${baseUrl}${btnSuccess}`); //Expects the browser to have the URL according the parameters passed to the function. If not, test will fail.
+        await browser.pause(3000); //Pause
+        await browser.back();//Goes back to the previous page (home)
         await this.btnLinkText.click();
         await expect(browser).toHaveUrl(`${baseUrl}${linkSuccess}`);
         await browser.pause(3000);
@@ -123,6 +123,13 @@ class Main extends Page {
         await expect(browser).toHaveUrl(`${baseUrl}${btnSuccess}/?button1=`);
         await browser.pause(3000);
         await browser.back();
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open (parameter) {
+        return super.open(parameter);
     }
 }
 
