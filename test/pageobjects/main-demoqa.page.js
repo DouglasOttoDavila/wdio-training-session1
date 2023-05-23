ebc07cf90134f589d55dd0e22589fdbeb0973d0e
DouglasOttoSf <douglas.davila@objectedge.com>
2023-05-23 15:36:44 -0300

    Updating spec files and adding file uploading check

--- test/pageobjects/main-demoqa.page.js
+++ test/pageobjects/main-demoqa.page.js
@@ -1,26 +1,137 @@
-const Page = require('./page');
+const Page = require('./page')
+const Key = require ('webdriverio')
 
 /**
  * sub page containing specific selectors and methods for a specific page
  */
 class Main extends Page {
 
+
     // CREATE YOUR SELECTORS HERE ↓ //
-    get btnId () {
-        return $(`//a[@id="idExample"]`);
+    get formFname () {
+        return $(`#firstName`);
+    }
+    get formLname () {
+        return $(`#lastName`);
+    }
+    get formEmail () {
+        return $(`#userEmail`);
+    }
+    get formMobNumber () {
+        return $(`#userNumber`);
+    }
+
+    get formBirthdayField () {
+        return $(`#dateOfBirthInput`);
+    }
+
+    get formBirthdayMonth () {
+        return $(`//select[@class="react-datepicker__month-select"]`);
+    }
+
+    get formBirthdayYear () {
+        return $(`//select[@class="react-datepicker__year-select"]`);
+    }
+
+    get formSubjectsInput () {
+        return $(`#subjectsInput`);
+    }
+
+    get formSubjectsField () {
+        return $(`#subjectsContainer`);
+    }
+
+    get formCurrentAddress () {
+        return $(`#currentAddress`);
+    }
+
+    get formState () {
+        return $(`#state`);
+    }
+
+    get formStateInput () {
+        return $(`#react-select-3-input`);
+    }
+
+    get formCity () {
+        return $(`#city`);
+    }
+
+    get formCityInput () {
+        return $(`#react-select-4-input`);
+    }
+
+    get uploadPicture () {
+        return $(`#uploadPicture`);	
+    }
+
+    get submitForm () {
+        return $(`#submit`);
     }
 
 
     // CREATE YOUR FUNCTIONS HERE ↓ //
-    async selectFromDropdown() {
-        console.log("|LOG| Started selecting Dropdown!");
-        var log1 = await this.dropdownSelect.getValue(); //Creates a variable to store the value of the dropdown, BEFORE changing it
-        console.log(`LOG MESSAGE 1: Dropdown Value is ` + log1); //Adds a console message printing the DEFAULT value of the dropdown ("Volvo")
-        await this.dropdownSelect.selectByAttribute('value', 'opel'); //Selects a new value from the dropdown, considering the attribute "value" and the attribute's value "opel"
-        log1 = await this.dropdownSelect.getValue(); //Creates a variable to store the value of the dropdown, AFTER it was changed
-        console.log(`LOG MESSAGE 2: Dropdown Value is ${log1}`); //Adds a console message printing the CURRENT value of the dropdown ("Opel")
-        console.log("|LOG| Dropdown has been selected!");
+    
+    async fillStudentForm(firstname, lastname, email, number, month, year, day, gender, subjects, path, address, state, city) {
+        
+        await this.formFname.setValue(firstname);
+        await this.formLname.setValue(lastname);
+        await this.formEmail.setValue(email);
+        await this.formMobNumber.setValue(number);
+        await this.setBirthday(month, year, day);
+        await this.setGender(gender);
+        await this.formSubjectsField.click();
+        await this.setSubjects(subjects);
+        await this.uploadFile(path);
+        await this.setState(state);
+        await this.setCity(city);
+        await this.formCurrentAddress.setValue(address);
+        await this.submitForm.click();
     }
+
+    async uploadFile(path) {
+        const remoteFilePath = await browser.uploadFile(path)
+        this.uploadPicture.setValue(remoteFilePath);
+    }
+
+    async setState(state) {
+        await this.formState.click();
+        await this.formStateInput.setValue(state);
+        await browser.pause(1000);
+        await browser.keys('Enter');
+    }
+
+    async setCity(city) {
+        await this.formCity.click();
+        await this.formCityInput.setValue(city);
+        await browser.pause(1000);
+        await browser.keys('Enter');
+    }
+
+    async setBirthday(month, year, day) {
+        await this.formBirthdayField.click();
+        await this.formBirthdayMonth.selectByVisibleText(month);
+        await this.formBirthdayYear.selectByVisibleText(year);
+        const birthdayDay = $(`//div[@class="react-datepicker__week"]/div[text()="${day}"]`);
+        await birthdayDay.click();
+    }
+
+    async setGender(gender) {
+        const genderSelect = $(`//label[text()="${gender}"]`);
+        await genderSelect.click();
+    }
+
+    async setSubjects (subjects) {
+        
+        for (let i = 0; i < subjects.length; i++) {
+            await this.formSubjectsInput.setValue(subjects[i]);
+            await browser.pause(1000);
+            await browser.keys('Enter');
+            
+        }
+        
+    }
+
 
     /**
      * overwrite specific options to adapt it to page object
--- test/specs/demoqa.e2e.js
+++ test/specs/demoqa.e2e.js
@@ -2,7 +2,23 @@ const Main = require('../pageobjects/main-demoqa.page')
 
 describe('Demo QA', () => {
     it('Demo QA - Forms', async () => {
-        await Main.open('text-box','demoqa');
+        await Main.open('automation-practice-form','demoqa');
+
+        await Main.fillStudentForm('Doug', 
+        'Davila', 
+        'douglas.davila@objectedge.com', 
+        '5551981270800', 
+        'November', 
+        '1990', 
+        '4', 
+        'Male',
+        ['English','Hindi'],
+        'test/images/photo.jpg',
+        'Col. Vincent, 625',
+        'NCR',
+        'Delhi');
+        
+        await browser.pause(5000);
     })
 })
 
